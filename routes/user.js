c62709231624782fe1ac2af7b6febab288e1a49c
Nika <freexnick@yahoo.com>
2022-10-03 16:55:43 +0400

    feat:user_rest

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { addUser, getUsers } from "../controllers/user.js";
+import {
+  addUser,
+  getUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.route("/").get(getUsers).post(addUser);
+
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
 export { router };
