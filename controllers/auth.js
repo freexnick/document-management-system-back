d7e23eb48209fdf6d29d6e6fb7fae8a8fb38bcc6
Nika <freexnick@yahoo.com>
2022-10-11 18:05:18 +0400

    refactor:auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,16 @@ import { User } from "../models/User.js";
 
 export const auth = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  const validatePassword = await user?.comparePasswords(password);
-  if (validatePassword) req.session.user = user?._id;
-  res.status(200).json(user);
+  try {
+    const user = await User.findOne({ email });
+    const validatePassword = await user?.comparePasswords(password);
+    if (validatePassword) {
+      req.session.user = user?._id;
+      res.status(200).json(user);
+    } else {
+      res.status(401).json({ status: 401, message: "invalid credentials" });
+    }
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e });
+  }
 };
