f00abe289f5998fc96990563952400c201a80c33
Nika <freexnick@yahoo.com>
2022-10-11 15:20:39 +0400

    refactor:auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,13 +4,6 @@ export const auth = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   const validatePassword = await user?.comparePasswords(password);
-  if (validatePassword) req.session.user = user.email;
-  res
-    .status(200)
-    .json({
-      status: 200,
-      message: "Success",
-      role: user.role,
-      userId: user.email,
-    });
+  if (validatePassword) req.session.user = user?._id;
+  res.status(200).json(user);
 };
